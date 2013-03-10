3c461749fbc78bdb9c69035e9994cd82d98d84d3
Dave King <dave@davbo.org>
2013-03-10 22:18:09 +0000

    Actually run things!

--- src/rush.rs
+++ src/rush.rs
@@ -1,15 +1,19 @@
-#[link_args="-ledit -ltermcap"]
-use libc::{c_char};
-extern mod edit {
-    fn el_init(argv: **c_char, stdin: io::Reader, stdout: io::Writer, stderr: io::Writer) -> *u8;
-    fn el_set(el: *u8, param: libc::c_int, val: ~str);
+extern mod linenoise;
+
+
+fn handle_input(line: ~str) {
+    linenoise::history_add(line);
+    linenoise::history_save("history.txt");
+    let split_line = str::split_char(line, ' ');
+    let command = split_line.head();
+    let args = split_line.tail();
+    core::run::run_program(*command, args);
 }
-pub const EL_EDITOR: libc::c_int = 2_i32;
+
 fn main() {
-    let args = os::args().map(|arg| str::as_c_str(*arg, |buf| buf));
-    unsafe {
-        let el = edit::el_init(vec::raw::to_ptr(args), io::stdin(), io::stdout(), io::stderr());
-        let editor = ~"vi";
-        edit::el_set(el, EL_EDITOR, editor);
+    linenoise::set_multiline(true);
+    loop {
+        let line = linenoise::init("rush: ");
+        handle_input(line);
     }
 }
