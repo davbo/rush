4181dbca8f8f76c5a8d5bc0c7ffc56a0c4e4e9e2
Dave King <dave@davbo.org>
2013-03-17 17:09:20 +0000

    Run each command as a separate task

--- src/rush.rs
+++ src/rush.rs
@@ -1,31 +1,50 @@
 extern mod linenoise;
 
+mod rush {
+    extern mod linenoise;
+    extern mod core;
+    use core::task::spawn;
+    use core::comm::{stream,Port,Chan,SharedChan};
 
+    fn write_str(wr: io::Writer, s: &str) { str::byte_slice(s, |v| wr.write(v)) }
 
-fn run_command(command: &~str) -> core::os::Pipe {
-    io::println(*command);
-    let pipe = core::os::pipe();
-    let command_split = str::split_char(*command, ' ');
-    let command_name = command_split.head();
-    let command_args = command_split.tail();
-    core::run::spawn_process(*command_name, command_args, ~None, ~None, pipe.in, pipe.out, pipe.out);
-    pipe
-}
-
-fn handle_input(line: &str) {
-    linenoise::history_add(line);
-    linenoise::history_save("history.txt");
-    let mut pipes = ~[];
-    let commands = str::split_char(line, '|');
-    for commands.each |command| {
-        pipes.push(run_command(command));
+    fn exec_command(command: &~str) -> Port<~str> {
+        let (strout,strin): (Port<~str>, Chan<~str>) = stream();
+        let com = copy *command;
+        do spawn {
+            let command_split = str::split_char(str::trim(com), ' ');
+            let prog = command_split.head();
+            let args = command_split.tail();
+            let mut prog = core::run::start_program(*prog, args);
+            let rd = prog.output();
+            let buf = io::with_bytes_writer(|wr| {
+                let mut bytes = [0, ..4096];
+                while !rd.eof() {
+                    let nread = rd.read(bytes, bytes.len());
+                    wr.write(bytes.view(0, nread));
+                }
+            });
+            strin.send(str::from_bytes(buf));
+        }
+        strout
     }
+
+    pub fn handle_input(line: &str) {
+        linenoise::history_add(line);
+        linenoise::history_save("history.txt");
+        let commands = str::split_char(line, '|');
+        let mut stdouts : ~[Port<~str>] = core::vec::map(commands, exec_command);
+        for stdouts.each |stdout| {
+            io::println(stdout.recv());
+        }
+    }
+
 }
 
 fn main() {
     linenoise::set_multiline(true);
     loop {
         let line = linenoise::init("rush: ");
-        handle_input(line);
+        rush::handle_input(line);
     }
 }
