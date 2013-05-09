d70713c43ba38e6f4fa55f2d328f22e95918539c
Dave King <dave@davbo.org>
2013-05-09 16:16:15 +0100

    Updated for rust 0.6
    Pipes working, for some reason they require a SingleThreaded scheduler

--- src/rush.rs
+++ src/rush.rs
@@ -3,23 +3,30 @@ extern mod linenoise;
 mod rush {
     extern mod linenoise;
     extern mod core;
-    use core::task::spawn;
     use core::run;
 
-    pub fn exec_command(stdin_fd: libc::c_int, command: &~str) -> libc::c_int {
+    pub fn exec_command(stdin_fd: libc::c_int, command: &str) -> libc::c_int {
         let pipe_out = os::pipe();
         let pipe_err = os::pipe();
-        let com = copy *command;
-        do spawn{
-            let command_split = str::split_char(com, ' ');
-            let prog = command_split.head();
-            let args = command_split.tail();
-            let pid = run::spawn_process(
-                    *prog, args, &None, &None,
-                    stdin_fd, pipe_out.out, pipe_err.out);
-            os::close(pipe_out.out);
-            os::close(pipe_err.out);
-            let status = run::waitpid(pid);
+        let mut args: ~[~str] = ~[];
+        let mut prog = ~"";
+        let mut first = true;
+        str::each_split_char(command, ' ', |item| {
+            if (first) {
+                prog = copy item.to_owned();
+                first = false;
+            } else {
+                args.push(item.to_owned());
+            }
+            true
+        });
+        let pid = run::spawn_process(
+                prog, args, &None, &None,
+                stdin_fd, pipe_out.out, pipe_err.out);
+        os::close(pipe_out.out);
+        os::close(pipe_err.out);
+        do task::spawn_sched(task::SingleThreaded) || {
+            run::waitpid(pid);
         }
         pipe_out.in
     }
@@ -27,16 +34,16 @@ mod rush {
     pub fn handle_input(line: &str) {
         linenoise::history_add(line);
         linenoise::history_save("history.txt");
-        let commands = vec::map(str::split_char(line, '|'), |command| str::trim(*command));
+        let mut commands: ~[&str] = ~[];
+        str::each_split_char_no_trailing(line, '|', |command| { commands.push(str::trim(command)); true });
         let pipe_in = os::pipe();
-        let std_out = vec::foldl(pipe_in.in, commands, exec_command);
+        let std_out = vec::foldl(pipe_in.in, commands, |stdin, command| {
+            exec_command(stdin, *command)
+        });
         os::close(pipe_in.out);
-        let (stdout_po, stdout_ch) = comm::stream();
         do task::spawn_sched(task::SingleThreaded) || {
-            stdout_ch.send(core::run::readclose(std_out));
+            io::print(core::run::readclose(std_out));
         }
-        let stdout = stdout_po.recv();
-        io::print(stdout);
     }
 
 }
